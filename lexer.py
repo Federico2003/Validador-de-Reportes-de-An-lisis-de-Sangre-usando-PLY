7323ebe33e304eb10a76065cf8b325eb2ee2eef4
Federico2003 <federico.zaragoza.manuel.1a@gmail.com>
2025-11-27 16:35:36 -0600

    Carpeta y lexer.py

---
+++ lexer.py
@@ -0,0 +1,73 @@
+#importamos librerias
+import ply.lex as lex
+import re
+
+# Lista de tokens
+tokens = (
+    'LBRACE', 
+    'RBRACE', 
+    'LBRACKET',
+    'RBRACKET',
+    'COLON',
+    'COMMA',
+    'STRING', 
+    'NUMBER'
+)
+
+# Tokens simples con expresiones regulares directas
+t_LBRACE    = r'\{'
+t_RBRACE    = r'\}'
+t_LBRACKET  = r'\['
+t_RBRACKET  = r'\]'
+t_COLON     = r':'
+t_COMMA     = r','
+
+# -------------------------
+# STRING: "texto"
+# Acepta caracteres normales y escapes tipo \" \n etc.
+# -------------------------
+def t_STRING(t):
+    r'"([^"\\]|\\.)*"'
+    # quitar comillas
+    t.value = t.value[1:-1]
+    return t
+
+# -------------------------
+# NUMBER: solo enteros (para este proyecto basta)
+# -------------------------
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Ignorar espacios, tabs y saltos
+t_ignore = ' \t\n\r'
+
+# -------------------------
+# Manejo de errores léxicos
+# -------------------------
+def t_error(t):
+    print(f"Caracter no permitido: {t.value[0]}")
+    t.lexer.skip(1)
+
+# Construir lexer
+lexer = lex.lex()
+
+data = '''
+    {
+  "folio": 15502427,
+  "fecha_toma": "14/06/2020 07:51:57",
+  "fecha_validacion": "14/06/2020 17:08:05",
+  "paciente": {
+    "nombre": "Ramírez Guzmán, María",
+    "fecha_nacimiento": "25/04/1985",
+    "sexo": "F",
+    "edad": 35
+  }
+}
+    '''
+
+lexer.input(data)
+
+for tok in lexer:
+    print(tok)
